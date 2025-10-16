fb509386584b6d9665924272f8b51282953e2f21
Jhon <jhon.zuluaga@uao.edu.co>
2025-10-16 12:40:23 -0500

    correcciones al frontend

--- FrontEnd/neoCDT-FRONT/src/App.jsx
+++ FrontEnd/neoCDT-FRONT/src/App.jsx
@@ -1,8 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomeScreen from "./components/homescreen";
 import Login from "./components/login";
 import Register from "./components/register";
 import ForgotPassword from "./components/forgotPassword";
+import SolicitudesPage from "./pages/SolicitudesPage";
+
+import "./css/global.css";
+import "./css/table.css";
+import "./css/form.css";
+import "./css/modal.css";
+import "./css/filterbar.css";
 
 
 function App() {
@@ -16,6 +23,8 @@ function App() {
        <Route path="/register" element={<Register />} />
 
        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+       <Route path="/solicitudes" element={<SolicitudesPage />} />
 
 
     </Routes>
