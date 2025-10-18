5d855660b3abd1d8d63e53da441263b7e551d80c
JAUM519 <jorgeamedinau@gmail.com>
2025-10-18 01:47:29 -0500

    .

--- FrontEnd/neoCDT-FRONT/vite.config.js
+++ FrontEnd/neoCDT-FRONT/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    test: {
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.test.jsx'], 
-    setupFiles: './src/setupTests.js'
-  }
 })
-
